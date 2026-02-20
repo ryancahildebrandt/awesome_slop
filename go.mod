fb750ffabc2d594727fea59b5456342a8cc0a552
ryancahildebrandt <ryancahildebrandt@gmail.com>
2026-02-19 20:04:00 -0500

    templating from yaml

---
+++ go.mod
@@ -0,0 +1,7 @@
+module awesome_slop
+
+go 1.24.4
+
+require (
+	github.com/goccy/go-yaml v1.19.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,71 @@
+// -*- coding: utf-8 -*-
+
+// Created on Tue Feb 17 09:53:08 PM EST 2026
+// author: Ryan Hildebrandt, github.com/ryancahildebrandt
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"text/template"
+
+	"github.com/goccy/go-yaml"
+)
+
+type Article struct {
+	Id       int64
+	Category string `yaml:"category"`
+	Title    string `yaml:"title"`
+	Url      string `yaml:"url"`
+	Abstract string `yaml:"abstract"`
+}
+
+func main() {
+	var articles []Article
+	var err error
+
+	b, err := os.ReadFile("./articles.yaml")
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	err = yaml.Unmarshal(b, &articles)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	for i := range articles {
+		articles[i].Id = int64(i)
+	}
+
+	funcMap := template.FuncMap{
+		"categoryReferences": func(v string) string {
+			var refs []string
+			for _, a := range articles {
+				if a.Category == v {
+					refs = append(refs, fmt.Sprintf("[%v](#%v)", a.Id, a.Id))
+				}
+			}
+			return fmt.Sprint("[", strings.Join(refs, ", "), "]")
+		}}
+	tmpl, err := template.New("main").Funcs(funcMap).ParseGlob("./templates/*.md.tmpl")
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	res, err := os.OpenFile("./README.md", os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	w := bufio.NewWriter(res)
+	err = tmpl.ExecuteTemplate(w, "main", articles)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	err = w.Flush()
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+}
